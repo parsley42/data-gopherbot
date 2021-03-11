ce5066d15235cc28b0c4accd660adf8899bc1348
David Parsley <parsley@linuxjedi.org>
2021-03-11 14:28:57 +0000

    Create .bashrc in job

--- jobs/theia.sh
+++ jobs/theia.sh
@@ -12,5 +12,12 @@ USE_LOCAL_GIT=true
 
 export SHELL THEIA_DEFAULT_PLUGINS USE_LOCAL_GIT
 
+cat > $HOME/.bashrc <<EOF
+# File created by jobs/theia.sh
+PS1="\[\033[01;32m\]robot@gopherbot-dev\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$ "
+PATH=$HOME/bin:$HOME/.local/bin:$HOME/go/bin:/opt/gopherbot:$PATH
+export PATH PS1
+EOF
+
 cd /usr/local/theia
 exec node /usr/local/theia/src-gen/backend/main.js /home/robot --hostname 0.0.0.0
